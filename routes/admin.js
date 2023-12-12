6487abba71b22073a6917ef510e95e20b1003f62
shsindnf <meshalkina.polina.2003@gmail.com>
2023-12-12 12:48:04 +0300

    third

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,12 @@ var router = express. Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const jsonParser = express.json();
+
 //Контроллеры из ветки моделей
 router.get ('/' ,adminController.getArticleAll); 
 router.get ('/:idArticle' ,adminController.getArticle) 
 router.post ('/addArticle',adminController.addArticle); 
 router.post ('/editArticle',adminController.editArticle); 
 router.post (' /deleteArticle',adminController.deleteArticle);
+
 module.exports = router;
