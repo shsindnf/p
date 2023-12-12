e151fc2a4a24e3ff40288ef67abcae31040b7c81
shsindnf <meshalkina.polina.2003@gmail.com>
2023-12-12 10:43:56 +0300

    first

---
+++ routes/article.js
@@ -0,0 +1,11 @@
+var express = require( 'express');
+const articleController = require("../controllers/articleController.js");
+var router = express. Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = express-json();
+
+router.get('/' ,articleController.getArticleAll);
+router.get('/:idArticle',articleController.getArticle)
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+const jsonParser = express.json();
+const homeController = require("../controllers/homeController.js");
+const { route } = require('./admin');
+
+router.get('/',homeController.getIndex);
+n
+module.exports = router;
